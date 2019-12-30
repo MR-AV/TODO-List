10c9feab4616157b149a482fbace70929ef9557b
MR.AV <akashvermaa0208@gmail.com>
2019-12-30 12:42:26 +0530

    TODO List

---
+++ date.js
@@ -0,0 +1,11 @@
+// rememeber  method.exports=exports
+exports.getDate = function() {
+  const today = new Date();
+  const options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+  }
+  return today.toLocaleDateString("en-US", options);
+}
